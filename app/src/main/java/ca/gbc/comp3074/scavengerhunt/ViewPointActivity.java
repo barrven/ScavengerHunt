2716cbf11d931dac8985bbc9d5b79509553eb8e1
Barrington <barrven@users.noreply.github.com>
2020-11-20 19:25:24 -0500

    updated delete point to refresh list after delete

--- app/src/main/java/ca/gbc/comp3074/scavengerhunt/ViewPointActivity.java
+++ app/src/main/java/ca/gbc/comp3074/scavengerhunt/ViewPointActivity.java
@@ -4,6 +4,7 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.net.Uri;
@@ -16,6 +17,9 @@ import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class ViewPointActivity extends AppCompatActivity {
 
     private DatabaseHelper dbHelper;
@@ -23,6 +27,10 @@ public class ViewPointActivity extends AppCompatActivity {
     private int id;
     private double ratings;
     private Point point;
+    private PointAdapter adapter;
+
+    //for refreshing list when deleting
+    private List<Point> points = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,10 +47,15 @@ public class ViewPointActivity extends AppCompatActivity {
         task = intent.getStringExtra("task");
         tags = intent.getStringExtra("tags");
         ratings = 0.0;
-        if(intent.getStringExtra("ratings") != null && intent.getStringExtra("ratings").length() > 0){ ratings = Double.parseDouble(intent.getStringExtra("ratings")); }
+        if(intent.getStringExtra("ratings") != null && intent.getStringExtra("ratings").length() > 0){
+            ratings = Double.parseDouble(intent.getStringExtra("ratings"));
+        }
         point = new Point(id,name,address,task,tags,ratings);
         tv.setText(point.getAddress() + "");
+
+        //initialize these for use with clearing list of items on delete
         dbHelper = new DatabaseHelper(this);
+        adapter = new PointAdapter(this, points, dbHelper);
     }
 
 
@@ -87,7 +100,9 @@ public class ViewPointActivity extends AppCompatActivity {
                     case DialogInterface.BUTTON_POSITIVE:
                         Toast.makeText(getApplicationContext(), "Item deleted", Toast.LENGTH_SHORT).show();
                         dbHelper.deleteItem(point);
-                        finish();
+                        //finish();
+                        clearList();
+                        backToView();
                         break;
                     case DialogInterface.BUTTON_NEGATIVE:
                         //Toast.makeText(getApplicationContext(), "nothing", Toast.LENGTH_SHORT).show();
@@ -97,6 +112,17 @@ public class ViewPointActivity extends AppCompatActivity {
         };
         AlertDialog.Builder builder = new AlertDialog.Builder(this);
         builder.setMessage("Are you sure you want to delete this point?").setPositiveButton("Delete point", diOnClick).setNegativeButton("No", diOnClick).show();
+    }
+
+    private void clearList(){
+        points.clear();
+        points.addAll(dbHelper.getAllItems());
+        adapter.notifyDataSetChanged();
+    }
+
+    private void backToView(){
+        Intent backToMain = new Intent(getApplicationContext(), MainActivity.class);
+        startActivity(backToMain);
     }
 
     private void openShowLocation(){
