196cd6cdfd2d03297e7d8c5fb98f47fe3dd2d7fc
Barrington <barrven@users.noreply.github.com>
2020-11-22 15:29:38 -0500

    coded search form ui

--- app/src/main/java/ca/gbc/comp3074/scavengerhunt/SearchActivity.java
+++ app/src/main/java/ca/gbc/comp3074/scavengerhunt/SearchActivity.java
@@ -3,12 +3,30 @@ package ca.gbc.comp3074.scavengerhunt;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class SearchActivity extends AppCompatActivity {
+
+    Spinner searchBy;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search);
+
+
+        searchBy = (Spinner) findViewById(R.id.sp_search_by);
+
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.search_choices, android.R.layout.simple_spinner_item);
+
+        // Specify the layout to use when the list of choices appears
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        // Apply the adapter to the spinner
+        searchBy.setAdapter(adapter);
+
+
+
     }
 }
