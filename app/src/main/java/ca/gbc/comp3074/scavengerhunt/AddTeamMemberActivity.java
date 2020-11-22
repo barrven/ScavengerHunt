cab2ecb8ad57e477fdb5d119963209694c2f246c
Barrington <barrven@users.noreply.github.com>
2020-11-22 18:21:18 -0500

    removed redundant code

--- app/src/main/java/ca/gbc/comp3074/scavengerhunt/AddTeamMemberActivity.java
+++ app/src/main/java/ca/gbc/comp3074/scavengerhunt/AddTeamMemberActivity.java
@@ -17,30 +17,5 @@ public class AddTeamMemberActivity extends AppCompatActivity {
         setContentView(R.layout.activity_add_team_member);
     }
 
-
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        MenuInflater inf = getMenuInflater();
-        inf.inflate(R.menu.team_menu, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
-        switch (item.getItemId()){
-            case R.id.menu_add_team_member:
-                openAddTeamMember();
-                return true;
-
-            default:return super.onOptionsItemSelected(item);
-        }
-    }
-
-    private void openAddTeamMember(){
-        Intent start = new Intent(getApplicationContext(), AddPointActivity.class);
-        startActivity(start);
-    }
-
     
 }
