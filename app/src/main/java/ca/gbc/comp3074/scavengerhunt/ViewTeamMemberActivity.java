00a368f7e04b6c532abe5f03c2cea360228225ec
Barrington <barrven@users.noreply.github.com>
2020-12-13 18:10:27 -0500

    minor adjustments to the layout of viewTeamMember

--- app/src/main/java/ca/gbc/comp3074/scavengerhunt/ViewTeamMemberActivity.java
+++ app/src/main/java/ca/gbc/comp3074/scavengerhunt/ViewTeamMemberActivity.java
@@ -7,6 +7,8 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -18,10 +20,6 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class ViewTeamMemberActivity extends AppCompatActivity {
-
-    private int id;
-    private String name, email, phone, sms;
-    private double ratings;
 
     private TeamMember teammember;
     private TeamMemberAdapter adapter;
@@ -36,15 +34,7 @@ public class ViewTeamMemberActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_view_teammember);
 
-        // Receive info about object from previous activity
-        Intent intent = getIntent();
-        id = Integer.parseInt(intent.getStringExtra("id"));
-        name = intent.getStringExtra("name");
-        email = intent.getStringExtra("email");
-        phone = intent.getStringExtra("phone");
-        sms = intent.getStringExtra("sms");
-
-        teammember = new TeamMember(id,name,email,phone,sms);
+        teammember = getTeammemberFromIntent(getIntent());
 
         //initialize these for use with clearing list of items on delete
         dbHelper = new DatabaseHelper(this);
@@ -58,13 +48,33 @@ public class ViewTeamMemberActivity extends AppCompatActivity {
                 findViewById(R.id.tv_sms)
         };
 
-        outputs[0].setText(name);
-        outputs[1].setText(email);
-        outputs[2].setText(phone);
-        outputs[3].setText(sms);
+        outputs[0].setText(teammember.getName());
+        outputs[1].setText(teammember.getEmail());
+        outputs[2].setText(teammember.getPhone());
+        outputs[3].setText(teammember.getSms());
+
+
+        Button btnEditTeamMember = findViewById(R.id.btn_edit_team_member);
+        btnEditTeamMember.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
     }
 
-    //?
+    private TeamMember getTeammemberFromIntent(Intent intent){
+
+        // Receive info about object from previous activity
+        int id = Integer.parseInt(intent.getStringExtra("id"));
+        String name = intent.getStringExtra("name");
+        String email = intent.getStringExtra("email");
+        String phone = intent.getStringExtra("phone");
+        String sms = intent.getStringExtra("sms");
+
+        return new TeamMember(id,name,email,phone,sms);
+    }
+
     private void openDelete(){
         DialogInterface.OnClickListener diOnClick = new DialogInterface.OnClickListener() {
             @Override
@@ -73,19 +83,21 @@ public class ViewTeamMemberActivity extends AppCompatActivity {
                     case DialogInterface.BUTTON_POSITIVE:
                         Toast.makeText(getApplicationContext(), "Team member deleted", Toast.LENGTH_SHORT).show();
                         dbHelper.deleteItem(teammember);
-                        //finish();
                         clearList();
                         backToView();
                         break;
                     case DialogInterface.BUTTON_NEGATIVE:
-                        //Toast.makeText(getApplicationContext(), "nothing", Toast.LENGTH_SHORT).show();
+                        Toast.makeText(getApplicationContext(), "Cancelled delete", Toast.LENGTH_SHORT).show();
                         break;
                 }
             }
         };
         AlertDialog.Builder builder = new AlertDialog.Builder(this);
-        builder.setMessage("Are you sure you want to delete this team member?").setPositiveButton("Delete teammember", diOnClick).setNegativeButton("No", diOnClick).show();
+        builder.setMessage("Are you sure you want to delete this team member?")
+                .setPositiveButton("Delete teammember", diOnClick)
+                .setNegativeButton("No", diOnClick).show();
     }
+
     @Override
     public boolean onOptionsItemSelected(@NonNull MenuItem item) {
         switch (item.getItemId()){
